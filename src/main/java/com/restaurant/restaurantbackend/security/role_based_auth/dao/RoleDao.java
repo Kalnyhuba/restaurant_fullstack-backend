67285a0c067058e3d842e2d7b23c8909ff8b8e59
Kalnyhuba <huba.kalny3@gmail.com>
2023-03-11 17:19:25 +0100

    Modified most of the classes, implemented cookies to store tokens and login information.

--- src/main/java/com/restaurant/restaurantbackend/security/role_based_auth/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.restaurantbackend.security.role_based_auth.dao;
-
-import com.restaurant.restaurantbackend.security.role_based_auth.entity.Role;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleDao extends CrudRepository<Role, String> {
-}
